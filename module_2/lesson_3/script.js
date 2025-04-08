c2e0f49575a7e96baa51c58888f711ae80771985
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2025-04-08 22:40:08 +0300

    lesson_3

---
+++ module_2/lesson_3/script.js
@@ -0,0 +1,55 @@
+// -------------------------------------- 6
+
+/*
+ * Напиши скрипт перевірки підписки користувача при доступі до контента
+ * - Є три типа підписки: free, pro і vip.
+ * - Отримати доступ можуть тільки користувачі pro і vip
+ */
+
+// const sub = "vip";
+// let canConnect;
+
+// //         false  || true
+// if(sub === "pro" || sub === "vip") {
+//     canConnect = true;
+// } else {
+//     canConnect = false;
+// }
+
+//                  
+// let canConnect = sub === "pro" || sub === "vip";
+
+// console.log("canConnect", canConnect);
+
+
+
+
+
+
+
+
+
+
+/*
+ * Напиши скрипт, який перевіряє можливість відкрити чат з користувачем.
+ * Для цього, користувач має бути:
+ * - другом
+ * - онлайн
+ * - без режима не турбувати
+ */
+
+
+// const isOnline = true;
+// const isFriend = true;
+// const isDnd = true; 
+
+// const mute = isDnd ? "off" : "on";
+
+// const canOpenChat = isOnline && isFriend && !isDnd;
+ 
+// console.log("canOpenChat", canOpenChat);
+
+
+
+
+
